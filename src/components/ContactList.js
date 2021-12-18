64fcea7d4846d67577d92b62e55e9a4d24bf749d
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-18 17:05:44 +0530

    feat - del. operation

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -4,9 +4,19 @@ import ContactCard from "./ContactCard";
 const ContactList = (props) => {
   // console.log(props);
 
+  const deleteContactHandler = (id) => {
+    props.getContactId(id);
+  };
+
   const renderContactList = props.contacts.map((contact) => {
     console.log(contact.name);
-    return <ContactCard contact={contact} />;
+    return (
+      <ContactCard
+        contact={contact}
+        clickHandler={deleteContactHandler}
+        key={contact.id}
+      />
+    );
   });
 
   return <div> {renderContactList} </div>;
